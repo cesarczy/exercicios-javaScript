e0e6a92105b3e41084258d6c7a859ffc10c39775
Cesar Siqueira <cesarczy@gmail.com>
2022-10-24 17:55:06 -0300

    Add files via upload
    Exercício maior menor

---
+++ exe02.js
@@ -0,0 +1,44 @@
+/* //exemplo 1
+function max(x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+console.log(max(2, 6)); */
+
+/* //exemplo 2
+
+function max(x, y) {
+    if (x > y) {
+        return x;
+    } return y;
+}
+console.log(max(2, 6)); */
+
+/* //exemplo 3
+
+function max(x, y) {
+    if (x > y) return x;
+    return y;
+}
+console.log(max(2, 6)); */
+
+/* // exemplo 4
+function max(x, y) {
+    return x > y ? x : y;
+}
+console.log(max(2, 6)); */
+
+/* // exemplo 5
+
+const max = (x, y) => {
+    return x > y ? x : y;
+}
+console.log(max(100, 20)); */
+
+//exemplo 6
+
+const max = (x, y) => x > y ? x : y;
+console.log(max(100, 20));
